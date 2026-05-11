babf9898bbf939548cc1431ff0406c991298983f
nastya2508 <nastusiaok.2203@gmail.com>
2026-05-12 02:23:55 +0300

    Update API URL to Render

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/', // Адреса твого бекенду
+  baseURL:  https://herofit-backend-2.onrender.com, // Адреса твого бекенду
 });
 
 // 1. Перед кожним запитом додаємо Access Token
